c4a0bf930b118246f263a6a7f71faa3a5b687a04
Gabriellfaria23 <124360805+Gabriellfaria23@users.noreply.github.com>
2023-02-03 13:39:54 -0300

    Add files via upload

---
+++ TiposPrimitivos.java
@@ -0,0 +1,21 @@
+public class TiposPrimitivos {
+
+    public static void main(String[] args) {
+        // int, double, gloat, char, byte, short, long, boolean
+
+        int idade = 10;
+        double salarioDouble = 2800;
+        float salarioFloat = 2500;
+        byte idadeByte = 10;
+        short idadeShort = 10;
+        boolean verdadeiro = true;
+        char caractere = 'M';
+        String nome = "um grande texto";
+        System.out.println("a idade é "+idade+ " anos.");
+
+    }
+
+
+
+}
+
---
+++ tiposPrimitivoExercicio.java
@@ -0,0 +1,18 @@
+public class tiposPrimitivoExercicio {
+
+    public static void main(String[] args) {
+
+/* Eu <nome>, morando no endereço <endereço> confirmo que recebi o salário de <salário>, na data <data>*/
+
+        String nome = "Gabriel Araújo Faria";
+        String endereço = "Rua antonio Teixeira Dias";
+        double salario = 4000;
+        String dataRecebimentoSalario = "05/01/2022";
+
+        System.out.println("Eu "+nome+ " morando no endereço "+endereço+ " confirmo que recebi o salário de "+salario+
+        " na data " +dataRecebimentoSalario+ ".");
+
+        // digitar "sout" para incluir System.out.println//
+
+    }
+}
