6fc889d732d0c3e98f7f21483bb3fac46d40e0c8
gabrielfaria <gabriellfaria23@gmail.com>
2023-02-06 17:29:24 -0300

    TesteInicial2

--- src/EstruturaDeRepetição01.java
+++ src/EstruturaDeRepetição01.java
@@ -2,18 +2,24 @@ public class EstruturaDeRepetição01 {
 
     public static void main(String[] args) {
         //while, do while, for
-        int count = 12;
-        while(count < 10) {
-            System.out.println(++count);}
+        int count = 0;
+        while (count < 10) {
+            System.out.println(++count);
+        }
 
         //Sem o "++count"  o código ficará contando zeros sem parar, o while executa a condição apenas se for compativel.
-
+          count = 0;
         do {
-            System.out.println("Dentro do do-while");
-        }while (count < 13);
+            System.out.println("Dentro do do-while "+ ++count);
+        } while (count < 10);
 
-        // O "do while" executa a condição de qualquer forma e depois avalia se continuará executando.
+        /* O "do while" executa a condição de qualquer forma e depois avalia se continuará executando, " + ++count" é
+        para o do while imprimir ate o número dez e parar.*/
 
-        for(int i;i<10;i++){
+        for(int i=0 ;i < 11; i++) {
+            System.out.println("for "+i  ); }
+        /*Declarei uma variavel dentro do for informei o valor 11 que irá imprimir ate o dez e o "i++" para parar de
+        rodar.*/
+    }
+     }
 
-}      }
---
+++ src/EstruturaDeRepetição03.java
@@ -0,0 +1,6 @@
+public class EstruturaDeRepetição03 {
+    public static void main(String[] args) {
+        for (int i = 1)
+
+    }
+}
